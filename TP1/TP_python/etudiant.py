d44e3d54546a77381022020748f2d228e97e0016
ErwynOfficial <erwyn.richer@ensta.fr>
2026-04-02 11:30:48 +0200

    re_tp_python

--- TP1/TP_python/etudiant.py
+++
@@ -1,9 +0,0 @@
-from personne import Personne
-
-class Etudiant(Personne):
-
-    def __init__(self, nom, age, numero, moyenne):
-        super().__init__(nom, age)
-        self.numero = numero
-        self.cours = []
-        self.moyenne = moyenne
--- TP1/TP_python/test_cours.py
+++
@@ -1,6 +0,0 @@
-from cours import Cours
-
-def test_cours():
-    cours = Cours("Conception Logiciel", "M. Ba")
-    assert cours.nom == "Conception Logiciel"
-    assert cours.professeur == "M. Ba"
